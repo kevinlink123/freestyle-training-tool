d6e16a2ded530f203bb91e5345d779f939115785
Kevin Ybarra <kevin.ybarra@gm2dev.com>
2021-03-08 17:04:26 -0300

    Correccion de errores -Ahora se pueden usar links del acortador de youtube correctamente -Ahora se puede usar un link con datos del segundo al que reproducirse o viene de una lista de reproduccion (Estos son eliminados)

--- js/beatChanger.js
+++ js/beatChanger.js
@@ -7,8 +7,17 @@ function cargarLink() {
   var YTLink = document.getElementById("url-input").value;
   var player = document.getElementById("player");
   var index = YTLink.indexOf("=") + 1;
+  if (YTLink.indexOf(".be") != -1) {
+    console.log("Es un link acortado");
+    index = YTLink.indexOf(".be") + 4;
+  }
   console.log(index);
-  YTLink = YTLinkStart + YTLink.slice(index);
+  var youtubeLinkId = YTLink.slice(index);
+  if (youtubeLinkId.indexOf("&") != -1) {
+    var auxIndex = youtubeLinkId.indexOf("&");
+    youtubeLinkId = youtubeLinkId.slice(0, auxIndex);
+  }
+  YTLink = YTLinkStart + youtubeLinkId;
   player.src = YTLink;
   console.log(YTLink);
 };
